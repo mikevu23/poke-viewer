3a6f2608c8dfa82ce0f66f91ee86c01fe3cc7b1a
mikevu23 <43010987+mikevu23@users.noreply.github.com>
2021-04-24 12:13:58 -0700

    Added prettier file for formatting on save, and created landingPage component

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container class="landingPage">
+    <v-row>
+      <v-text-field id="searchBar" v-model="searchFields"></v-text-field>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({})
+export default class LandingPage extends Vue {
+  searchFields = "fdsa";
+}
+</script>
+
+<style lang="sass"></style>
